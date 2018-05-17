38fac2fe098ac85f28967252b9950f73d68a2e3f
杨阳 <yangyang@rgbvr.com>
2018-05-17 19:07:06 +0800

    这路由整不明白了？

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, hashHistory, IndexRoute} from 'react-router'
+import { Router, Route, HashRouter, IndexRoute} from 'react-router'
 import './index.css'
 import App from './App'
 import Home from './components/home'
 import registerServiceWorker from './registerServiceWorker'
 ReactDOM.render(
-  <Home />,
+        <App />
+   ,
   document.getElementById('root'))
 
 // 主要是用于在生产环境中为用户在本地创建一个service worker 来缓存资源到本地，提升应用的访问速度
--- src/router.js
+++ src/router.js
@@ -4,45 +4,25 @@ import {
     Route,
     Link
 } from 'react-router-dom'
-import { Router, Route, IndexRoute, applyRouterMiddleware} from "react-router";
+
 import Home1 from "./components/home"
 
-
-function generatePageFunction(pageUrl) {
-    return (nextState, cb) => {
-        require.ensure([], require => {
-            // default import is not supported in CommonJS standard
-            cb(null, require(pageUrl).default)
-        });
-    };
-}
-
-/*
- const routes = [
- { path: '/',
- exact: true,
- sidebar: () => <div>home!</div>,
- main: () => <h2>Home</h2>
- },
- { path: '/bubblegum',
- sidebar: () => <div>bubblegum!</div>,
- main: () => <h2>Bubblegum</h2>
- },
- { path: '/shoelaces',
- sidebar: () => <div>shoelaces8!</div>,
- main: () => <h2>Shoelaces</h2>
- },
- { path: './home2',
- sidebar: () => <div>shoe1lace</div>,
- component: () => <Home1/>
- }
- ]
- */
-let LoginIndex = generatePageFunction("./components/home");
-let LoginIndex2 = generatePageFunction("./components/login");
-const Routes = (
-    <Router>
-        <IndexRoute getComponent={LoginIndex}/>
-    </Router>
-)
+const routes = [
+    {
+        path: "/",
+        exact: true,
+        sidebar: () => <div>home!</div>,
+        main: () => <h2>Home</h2>
+    },
+    {
+        path: "/bubblegum",
+        sidebar: () => <div>bubblegum!</div>,
+        main: () => <h2>Bubblegum</h2>
+    },
+    {
+        path: "/shoelaces",
+        sidebar: () => <div>shoelaces!</div>,
+        main: () => <h2>Shoelaces</h2>
+    }
+];
 export default routes
