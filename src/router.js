f389165c982fee0f35cfe6c29728f4009389d0f0
杨阳 <1002522146@qq.com>
2018-11-13 14:30:28 +0800

    fix： 优化函数暴露方式

--- src/router.js
+++ src/router.js
@@ -1,27 +1,6 @@
 import React from "react"
-import Loadable from 'react-loadable'
-import LoadingComponent from './components/loading/index'
-import Wrapper from './components/wrapper'
 import Untils from './untils'
 
-/**
- *
- * @param options {loader: () => import('')}
- * @loading loading 组件
- * @returns {*}
- */
-const asyncComponent = (options) => {
-  return Loadable({
-    loading: LoadingComponent,
-    render: (loaded, props) => {
-      const Component = loaded.default;
-      // 引入高阶组件
-      const WrappedComponent = Wrapper(Component);
-      return <WrappedComponent {...props}  />;
-    },
-    ...options
-  });
-};
 console.log(Untils)
 const routes = [
   {
@@ -30,14 +9,14 @@ const routes = [
     sidebar: () => <div>
       点击左边导航栏开始react
     </div>,
-    component: asyncComponent({
+    component: Untils.asyncComponent({
       loader: () => import('./components/defalut')
     })
   },
   {
     path: '/home',
     sidebar: () => <div>1</div>,
-    component: asyncComponent({
+    component: Untils.asyncComponent({
       loader: () => import('./components/defalut')
     })
 
@@ -45,14 +24,14 @@ const routes = [
   {
     path: '/bubblegum',
     sidebar: () => <div>2</div>,
-    component: asyncComponent({
+    component: Untils.asyncComponent({
       loader: () => import('./components/home/index')
     })
   },
   {
     path: '/shoelaces',
     sidebar: () => <div>3</div>,
-    component: asyncComponent({
+    component: Untils.asyncComponent({
       loader: () => import('./components/test')
     })
   }
--- src/untils.js
+++ src/untils.js
@@ -1,22 +1,41 @@
 import Loadable from 'react-loadable'
 import LoadingComponent from './components/loading/index'
 import Wrapper from './components/wrapper'
+import React from 'react'
 
+/**
+ *
+ * @returns {string} 随即在数组中取颜色
+ */
+const getRandomColor=()=> {
+  const bgcColor =['#C2FFD8','#465EFB','#F97794','#92FFC0','#002661','#F0FF00','#FFA8A8']
+  function  randomColor(){
+    return bgcColor[Math.floor(Math.random()*bgcColor.length)]
+  }
+  return `linear-gradient(to bottom right, ${randomColor()} , ${randomColor()})`
+};
 /**
  *
  * @param options {loader: () => import('')}
  * @loading loading 组件
  * @returns {*}
  */
-const until ={
-  getRandomColor:()=> {
-    //随机在数组取颜色
-    const bgcColor =['#C2FFD8','#465EFB','#F97794','#92FFC0','#002661','#F0FF00','#FFA8A8']
-    function  randomColor(){
-      return bgcColor[Math.floor(Math.random()*bgcColor.length)]
-    }
-    return `linear-gradient(to bottom right, ${randomColor()} , ${randomColor()})`
-  }
+
+const asyncComponent=(options) => {
+  return Loadable({
+    loading: LoadingComponent,
+    render: (loaded, props) => {
+      const Component = loaded.default;
+      // 引入高阶组件
+      const WrappedComponent = Wrapper(Component);
+      return <WrappedComponent {...props}  />;
+    },
+    ...options
+  });
 }
 
-export default until
+
+export default {
+  getRandomColor,
+  asyncComponent
+}
